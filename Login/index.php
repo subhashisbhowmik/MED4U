0265984a8607702735f56ab3f19af8d08ee45942
Sudipta <sudipta.banik.112@gmail.com>
2017-01-09 00:03:40 +0530

    login style changed

--- Login/index.php
+++ Login/index.php
@@ -5,7 +5,6 @@ if(isset($_COOKIE['username'])) //die($_COOKIE['username']);
 ?>
 
 <!DOCTYPE html>
-<!-- saved from url=(0035)http://medipack.ddns.net/med/Login/ -->
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -25,38 +24,28 @@ if(isset($_COOKIE['username'])) //die($_COOKIE['username']);
         <br>
         <h1 class="login-heading">
             <strong>Welcome.</strong></h1>
-        <!--form method="post" action="../login.php"-->
-        <div class="lnk warning" hidden="hidden"
-             style="color:#fd0;float:right;position: relative; padding-bottom: 0% ; font-size: 90%"><strong>Invalid
-                Username or Password</strong></div>
+        <form method="post" action="../login.php">
+        <div class="lnk warning" hidden="hidden" style="color:#fd0;float:right;position: relative; padding-bottom: 0% ; font-size: 90%"><strong>Invalid Username or Password</strong></div>
         <input style="display:none">
         <input type="password" style="display:none">
         <input type="text" name="username" id="username" placeholder="Username" required="required" class="input-txt">
 
-        <input type="text" style="-webkit-text-security: disc;" name="password" id="password" placeholder="Password"
-               required="required" class="input-txt">
-        <!--            <br/><span class="lnk" style="color:pink; float:right; margin-left:3%" hidden="hidden"></span>-->
+        <input type="password" style="-webkit-text-security: disc;" name="password" id="password" placeholder="Password" required="required" class="input-txt">
+        
         <div class="login-footer">
-            <a href="http://medipack.ddns.net/med/Login/#" class="lnk">
-                <!--span class="icon icon--min">ಠ╭╮ಠ</span-->
-                I've forgotten something
-            </a>
+                
 
-            <!--                <div class="sk-folding-cube loader" hidden="hidden" style="position: relative;float: right;zoom:0.5;margin-left: 12px;margin-right: 12px;">-->
-            <!--                    <div class="sk-cube1 sk-cube"></div>-->
-            <!--                    <div class="sk-cube2 sk-cube"></div>-->
-            <!--                    <div class="sk-cube4 sk-cube"></div>-->
-            <!--                    <div class="sk-cube3 sk-cube"></div>-->
-            <!--                </div>-->
-            <button type="submit" id="submit" class="btn btn--right" disabled="">Sign in</button>
-            <div class="spinner loader" hidden="hidden"
-                 style="position: relative;float: right;zoom:0.5;margin-left: 12px;margin-right: 12px; margin-top:3%;overflow: visible">
-                <div class="double-bounce1"></div>
-                <div class="double-bounce2"></div>
-            </div>
-            <div style="clear: both">
-            </div>
-            <!--/form-->
+                <button type="submit" id="submit" class="btn btn--right" disabled="">Sign in</button>
+                <div class="spinner loader" hidden="hidden" style="position: relative;float: right;zoom:0.5;margin-left: 12px;margin-right: 12px; margin-top:1%;overflow: visible">
+                    <div class="double-bounce1"></div>
+                    <div class="double-bounce2"></div>
+                </div>
+                    
+        </div>
+        <div class="login-footer">
+                <a href="http://medipack.ddns.net/med/Login/#" class="lnk">I've forgotten something</a>        
+        </div>
+        </form>
         </div>
     </div>
     <script src="../js/jq.js"></script>
